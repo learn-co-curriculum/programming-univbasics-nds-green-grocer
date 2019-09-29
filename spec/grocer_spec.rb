0834c83794bc2b8ae1be0c7a3f3def6cbfe5d6f8
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-29 13:24:10 -0400

    Remove bizaare Hash structure of cart

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -1,16 +1,17 @@
 describe "Grocer" do
+  # let() is like a global variable for tests
   let(:items) do
     [
-      {"AVOCADO" => {:price => 3.00, :clearance => true}},
-      {"KALE" => {:price => 3.00, :clearance => false}},
-      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
-      {"ALMONDS" => {:price => 9.00, :clearance => false}},
-      {"TEMPEH" => {:price => 3.00, :clearance => true}},
-      {"CHEESE" => {:price => 6.50, :clearance => false}},
-      {"BEER" => {:price => 13.00, :clearance => false}},
-      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-      {"BEETS" => {:price => 2.50, :clearance => false}},
-      {"SOY MILK" => {:price => 4.50, :clearance => true}}
+      {:item => "AVOCADO", :price => 3.00, :clearance => true},
+      {:item => "KALE", :price => 3.00, :clearance => false},
+      {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+      {:item => "ALMONDS", :price => 9.00, :clearance => false},
+      {:item => "TEMPEH", :price => 3.00, :clearance => true},
+      {:item => "CHEESE", :price => 6.50, :clearance => false},
+      {:item => "BEER", :price => 13.00, :clearance => false},
+      {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+      {:item => "BEETS", :price => 2.50, :clearance => false},
+      {:item => "SOY MILK", :price => 4.50, :clearance => true}
     ]
   end
 
